ff5a17a520161d836938c6c575bd1851437a3a0d
mcanmor0312 <mcanmor0312@g.educaand.es>
2024-02-23 13:52:22 +0100

    By Marcos

--- src/GestionEventosDeportivos/classes/impl/ParticipanteCarrera.java
+++ src/GestionEventosDeportivos/classes/impl/ParticipanteCarrera.java
@@ -1,5 +1,34 @@
 package GestionEventosDeportivos.classes.impl;
 
 public class ParticipanteCarrera extends Participante{
+    //declaramos las variables
     double tiempo;
+
+    //Declaramos getters y setters
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        if (tiempo>0) {
+            this.tiempo = tiempo;
+        }
+    }
+
+    public ParticipanteCarrera(String dni, String apellidos, String nombre, int edad, Double tiempo) {
+        super(dni, apellidos, nombre, edad);
+        setTiempo(tiempo);
+    }
+    //Creamos el toString
+    @Override
+    public String toString(){
+        String cadenaAMostrar="";
+
+        cadenaAMostrar+="Participante llamado: "+this.nombre+" "+this.apellidos+"\n";
+        cadenaAMostrar+="DNI: "+this.dni+"\n";
+        cadenaAMostrar+="Edad: "+this.edad;
+        cadenaAMostrar+="El tiemp del participante es de "+this.tiempo;
+
+        return cadenaAMostrar;
+    }
 }
